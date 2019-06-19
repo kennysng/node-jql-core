03ef80110d7f163df58495d6297fb1a5deb65352
Kennys Ng <kennysng@hotmail.com.hk>
2019-06-19 12:37:08 +0800

    fix InMemoryEngine

--- src/engine/memory/index.ts
+++ src/engine/memory/index.ts
@@ -11,8 +11,6 @@ import { CompiledQuery } from '../core/query'
 import { Sandbox } from '../core/sandbox'
 
 export class InMemoryEngine extends DatabaseEngine {
-  public readonly options?: IDatabaseOptions
-
   protected readonly schema = new Schema()
   protected readonly context: IDataSource = {}
   protected readonly functions = new Functions()
@@ -20,6 +18,10 @@ export class InMemoryEngine extends DatabaseEngine {
   private readonly schemaLock = new ReadWriteLock()
   private readonly databaseLocks = new ReadWriteLocks()
   private readonly tableLocks = new ReadWriteLocks()
+
+  constructor(public readonly options?: IDatabaseOptions) {
+    super()
+  }
 
   // @override
   get [Symbol.toStringTag](): string {
