ad4ae85eb1dba06641c4b3875a6711770b08b172
Kennys Ng <kennysng@hotmail.com.hk>
2019-07-17 09:19:49 +0800

    fix post-bundling script

--- script/post-process.ts
+++ script/post-process.ts
@@ -1,7 +1,9 @@
 export default function process(content: string[]): string[] {
-  const subContent = content.splice(5176, 24)
+  const oldIndex = content.findIndex(line => line.startsWith('abstract class CompiledExpression')) - 5
+  const newIndex = content.findIndex(line => line.startsWith('class CompiledColumnExpression')) - 5
+  const subContent = content.splice(oldIndex, 25)
   for (let i = 0, length = subContent.length; i < length; i += 1) {
-    content.splice(3007 + i, 0, subContent[i])
+    content.splice(newIndex + i, 0, subContent[i])
   }
   return content
 }
